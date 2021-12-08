190eb11e0a5c15e927d481e02f8f65feb80e8a2e
hutornow <hutornow@gmail.com>
2021-12-08 19:57:52 +0300

    Profile widget error

--- lib/presentation/pages/profile.dart
+++ lib/presentation/pages/profile.dart
@@ -5,6 +5,40 @@ class ProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        appBar: AppBar(
+      title: const Text('Профиль'),
+      actions: <Widget>[
+        IconButton(
+          icon: const Icon(Icons.account_circle_outlined),
+          tooltip: 'Show Snackbar',
+          onPressed: () {
+            ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(content: Text('This is a snackbar')));
+          },
+        ),
+        IconButton(
+          icon: const Icon(Icons.navigate_next),
+          tooltip: 'Go to the next page',
+          onPressed: () {
+            Navigator.push(context, MaterialPageRoute<void>(
+              builder: (BuildContext context) {
+                return Scaffold(
+                  appBar: AppBar(
+                    title: const Text('Next page'),
+                  ),
+                  body: const Center(
+                    child: Text(
+                      'This is the next page',
+                      style: TextStyle(fontSize: 24),
+                    ),
+                  ),
+                );
+              },
+            ));
+          },
+        ),
+      ],
+    ));
   }
 }
